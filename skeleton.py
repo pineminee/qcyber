ca3ded859ed00d02be564edb88ae88facd9e1418
pineminee <51804265+pineminee@users.noreply.github.com>
2019-06-13 23:35:43 -0400

    added comment

--- skeleton.py
+++ skeleton.py
@@ -9,6 +9,7 @@ def main():
             numbers are generated between 0 and 65535 (this is the max provided by the api: https://qrng.anu.edu.au/API/api-demo.php)
             I generate 223 numbers and get a summation of their numbers
             Then I add 223 to that sum
+            (because my index for the articles starts at 1)
     '''
     try:
         print('sending request to: https://qrng.anu.edu.au/API/jsonI.php?length=223&type=uint16')
