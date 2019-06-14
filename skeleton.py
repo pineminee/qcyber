f2b3cb6051a3526c823fd436822b05bd150461d2
pineminee <51804265+pineminee@users.noreply.github.com>
2019-06-13 23:32:22 -0400

    upload skeleton

---
+++ skeleton.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+
+def main():
+    '''
+            There are 14625793 wikipedia articles
+            numbers are generated between 0 and 65535 (this is the max provided by the api: https://qrng.anu.edu.au/API/api-demo.php)
+            I generate 223 numbers and get a summation of their numbers
+            Then I add 223 to that sum
+    '''
+    try:
+        print('sending request to: https://qrng.anu.edu.au/API/jsonI.php?length=223&type=uint16')
+        r = requests.get("https://qrng.anu.edu.au/API/jsonI.php?length=223&type=uint16").json()
+        print('response: ')
+        print(r)
+        rsum = sum(r['data'])
+        rsum = rsum + 223
+        print('sum: %i' % rsum)
+        print('There are 14625793 wikipedia articles')
+        print('%i will be mapped to an article' % rsum)
+    except Exception as e:
+
+        print("oops unexpected error")
+
+    return
+
+main()
