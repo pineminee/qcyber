b6cca347b8c3f3085e6b6e849c7dbdc6d454e1c6
pineminee <51804265+pineminee@users.noreply.github.com>
2019-06-13 23:54:54 -0400

    edited for more readability

--- skeleton.py
+++ skeleton.py
@@ -7,20 +7,20 @@ def main():
     '''
             There are 14625793 wikipedia articles
             numbers are generated between 0 and 65535 (this is the max provided by the api: https://qrng.anu.edu.au/API/api-demo.php)
-            I generate 223 numbers and get a summation of their numbers
-            Then I add 223 to that sum
-            (because my index for the articles starts at 1)
+            I generate 223 numbers and sum the numbers
+            Then I add 223 to that sum (because my index for the articles starts at 1)
+            Finally, I map the number to a wiki article
     '''
     try:
-        print('sending request to: https://qrng.anu.edu.au/API/jsonI.php?length=223&type=uint16')
+        
         r = requests.get("https://qrng.anu.edu.au/API/jsonI.php?length=223&type=uint16").json()
-        print('response: ')
-        print(r)
         rsum = sum(r['data'])
         rsum = rsum + 223
-        print('sum: %i' % rsum)
+        
+        print('random number: %i' % rsum)
         print('There are 14625793 wikipedia articles')
         print('%i will be mapped to an article' % rsum)
+        
     except Exception as e:
 
         print("oops unexpected error")
